41ba791d75b9fa54a15a1d529886f6e6ae64c92f
Leah Chen <leeewchen@gmail.com>
2022-07-05 21:30:17 -0700

    commit tree 7f2a38d5910b96328ae3b8a10e0f9a3d9f716da6

---
+++ app/controllers/campers_controller.rb
@@ -0,0 +1,23 @@
+class CampersController < ApplicationController
+
+    def index
+        campers= Camper.all 
+        render json: campers
+    end
+
+    def show 
+        camper = Camper.find_by!(id: params[:id])
+        render json: camper, serializer: CamperWithActivitiesSerializer
+    end
+
+    def create 
+        camper= Camper.create!(camper_params)
+        render json: camper, status: :created
+    end
+
+    private 
+
+    def camper_params
+        params.permit(:name, :age)
+    end
+end
